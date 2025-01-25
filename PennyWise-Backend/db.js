a9c78d634fce4b28121d1cbb88569dbf669539fe
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-01-25 16:56:46 +0530

    .

--- PennyWise-Backend/db.js
+++ PennyWise-Backend/db.js
@@ -1,13 +1,14 @@
-import { Client } from 'pg'; 
+import pkg from 'pg'; 
+const { Client } = pkg; 
 
 const db = new Client({
   connectionString: process.env.DATABASE_URL, 
   ssl: {
-    rejectUnauthorized: false,  // Necessary for Render's SSL connection
+    rejectUnauthorized: false,  // Required for Render's SSL connection
   },
 });
 
-// Connecting to the database
+
 db.connect()
   .then(() => console.log("Connected to PostgreSQL database"))
   .catch((err) => console.error("Connection error", err.stack));
