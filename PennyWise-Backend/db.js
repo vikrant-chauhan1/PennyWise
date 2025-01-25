51aa1df1428c3ec853a117cc58b0d4dd871adb47
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-01-25 17:06:14 +0530

    .

--- PennyWise-Backend/db.js
+++ PennyWise-Backend/db.js
@@ -1,13 +1,12 @@
-import pkg from 'pg'; 
-const { Client } = pkg; 
+import pkg from 'pg'; // CommonJS import
+const { Pool } = pkg;
 
-const db = new Client({
-  connectionString: process.env.DATABASE_URL, 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,  // Required for Render's SSL connection
   },
 });
-
 
 db.connect()
   .then(() => console.log("Connected to PostgreSQL database"))
